ae71568f253c58a8282bbab9f8af56470dfdf7b0
Iranga Umayantha <86459815+Irangaumayantha2001@users.noreply.github.com>
2025-05-11 11:10:13 +0530

    layout add

--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -7,23 +7,22 @@ import UserProfile from "../components/User/userCard";
 
 export default function DashboardLayout() {
   return (
-    <div className="flex h-screen bg-gray-50">
+     <div className="flex">
       <Sidebar />
-      <main className="flex-1 flex flex-col overflow-hidden">
+      <div className="flex-1 min-h-screen bg-gray-50">
         <Header />
-        <div className="flex flex-1 overflow-hidden">
-          <div className="flex-1 overflow-y-auto p-6 space-y-6">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-              <PerformanceChart />
-              <TaskList />
-            </div>
-          </div>
-          <div className="w-full max-w-sm border-l border-gray-200 p-6 overflow-y-auto">
-            <UserProfile />
-            <ActivityFeed />
-          </div>
-        </div>
-      </main>
+          <main className="p-6 grid grid-cols-1 lg:grid-cols-3 gap-6">
+  <div className="col-span-2 space-y-6">
+    <PerformanceChart />
+    <TaskList />
+  </div>
+  <div className="space-y-6">
+    <UserProfile />
+    <ActivityFeed />
+  </div>
+</main>
+      </div>
     </div>
+
   );
 }
